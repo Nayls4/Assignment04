02bb346182a8fa5473cebb8a38c3baa38dca0836
naylj6470 <naylj6470@HRH-IC0027325.wrdsb.ca>
2015-11-06 10:56:10 -0500

    commit

--- src/Game.java
+++ src/Game.java
@@ -51,66 +51,115 @@ public class Game {
         board.putPiece(rowD3, colD3, Color.YELLOW);
             
         while(true){
-        
-        //if 1 and 2 crash
-        if(rowD1 == rowD2 && colD1 == colD2){
-            d1.crash(); 
-            d2.crash();
-            board.removePiece(rowD1,colD1);
-            board.removePiece(rowD2,colD2);
-            rowC1 = rowD1;
-            colC1 = colD1;
-            board.putPiece(rowC1, colC1, Color.RED);         
-        }
-        //if 2 and 3 crash
-        else if(rowD2 == rowD3 && colD2 == colD3){
-            d2.crash();
-            d3.crash();
-            board.removePiece(rowD2,colD2);
-            board.removePiece(rowD3,colD3);
-            rowC2 = rowD2;
-            colC2 = colD2;
-            board.putPiece(rowC2, colC2, Color.RED);
-        }
-        //if 1 and 3 crash
-        else if(rowD1 == rowD3 && colD1 == colD3){
-            d1.crash();
-            d3.crash();
-            board.removePiece(rowD1,colD1);
-            board.removePiece(rowD3,colD3);
-            rowC3 = rowD3;
-            colC3 = colD3;
-            board.putPiece(rowC3, colC3, Color.RED);
-        }
-        
-//        if(rowD1 == row && colD1 == col || rowD2 == row && colD2 == col || rowD3 == row && colD3 == col || rowC1 == row && colC1 == col || rowC2 == row && colC2 == col || rowC3 == row && colC3 == col){
-//            doc.caught();
-//        }
-//        
-        
-        
-//        board.putPiece(row,col,Color.GREEN);
-        
-//        while(true){
-        Coordinate c = board.getClick();
-            System.out.println(c.getCol() + "   " + c.getRow());
-            // || c.getCol() == col+1 || c.getRow() == row-1 || c.getCol() == col-1 
-        if(c.getRow() == row+1 && c.getCol() == col+1 || c.getRow() == row-1 && c.getCol() == col-1 || c.getRow() == row+1 && c.getCol() == col-1 || c.getRow() == row-1 && c.getCol() == col+1 || c.getRow() == row+1 || c.getRow() == row-1 || c.getCol() == col+1 || c.getCol() == col-1){
-//            System.out.println("row,col" + c.getCol() + "   " + c.getRow());
-            //remove old piece
-            board.removePiece(row, col);
-            //figure out where they clicked
-            row = c.getRow();
-            col = c.getCol();
-            //put piece where clicked
-            board.putPiece(row, col, Color.GREEN);
-        }else{
+            //if 1 and 2 crash
+            if(rowD1 == rowD2 && colD1 == colD2){
+                d1.crash(); 
+                d2.crash();
+                board.removePiece(rowD1,colD1);
+                board.removePiece(rowD2,colD2);
+                rowC1 = rowD1;
+                colC1 = colD1;
+                board.putPiece(rowC1, colC1, Color.RED);         
+            }
+            //if 2 and 3 crash
+            else if(rowD2 == rowD3 && colD2 == colD3){
+                d2.crash();
+                d3.crash();
+                board.removePiece(rowD2,colD2);
+                board.removePiece(rowD3,colD3);
+                rowC2 = rowD2;
+                colC2 = colD2;
+                board.putPiece(rowC2, colC2, Color.RED);
+            }
+            //if 1 and 3 crash
+            else if(rowD1 == rowD3 && colD1 == colD3){
+                d1.crash();
+                d3.crash();
+                board.removePiece(rowD1,colD1);
+                board.removePiece(rowD3,colD3);
+                rowC3 = rowD3;
+                colC3 = colD3;
+                board.putPiece(rowC3, colC3, Color.RED);
+            }
+
+//            if(rowD1 == row && colD1 == col || rowD2 == row && colD2 == col || rowD3 == row && colD3 == col || rowC1 == row && colC1 == col || rowC2 == row && colC2 == col || rowC3 == row && colC3 == col){
+//                doc.caught();
+//                board.removePiece(row,col);
+//                board.removePiece(rowD1,colD1);
+//                rowC3 = rowD3;
+//                colC3 = colD3;
+//                board.putPiece(rowC3, colC3, Color.RED);
+//                
+//                System.out.println("caught");
+//            }
+
+
+            Coordinate c = board.getClick();
+            if(c.getRow() > row+1 || c.getRow() < row-1 || c.getCol() > col+1 || c.getCol() < col-1){
+                //remove old piece                
+                board.removePiece(row, col);
+                //figure out where they clicked
+                row = (int)(Math.random()*8);
+                col = (int)(Math.random()*8);
+                //put piece where clicked
+                board.putPiece(row, col, Color.GREEN);            
+                }else{
+                //remove old piece                
+                board.removePiece(row, col);
+                //figure out where they clicked
+                row = c.getRow();
+                col = c.getCol();
+                //put piece where clicked
+                board.putPiece(row, col, Color.GREEN); 
+            }
+
+
+
+            if(row-1 > rowD1){
+                //remove old piece                
+                board.removePiece(rowD1, colD1);
+                //figure out where they clicked
+                rowD1++;
+                //put piece where clicked
+                board.putPiece(rowD1, colD1, Color.YELLOW);
+            }else if(row-1 < rowD1){
+                //remove old piece                
+                board.removePiece(rowD1, colD1);
+                //figure out where they clicked
+                rowD1--;
+                //put piece where clicked
+                board.putPiece(rowD1, colD1, Color.YELLOW);
+            }else if(col > colD1){
+                //remove old piece                
+                board.removePiece(rowD1, colD1);
+                //figure out where they clicked
+                colD1++;
+                //put piece where clicked
+                board.putPiece(rowD1, colD1, Color.YELLOW);
+            }else if(col < colD1){
+                //remove old piece                
+                board.removePiece(rowD1, colD1);
+                //figure out where they clicked
+                colD1--;
+                //put piece where clicked
+                board.putPiece(rowD1, colD1, Color.YELLOW);
+            }
             
-        }
-        
-        
-        
-        
-        }                        
+            
+            
+            
+            
+            
+//            if(row > d1.getRow() || row < d1.getRow()){
+//                System.out.println("yes");
+//                //remove old piece                
+//                board.removePiece(rowD1, colD1);
+//                //figure out where to move
+//                rowD1 = c.getRow();
+//                //colD1 = colD1++;
+//                //move Dalek
+//                board.putPiece(rowD1, colD1, Color.YELLOW); 
+//            }
+        }    
     }
 }
